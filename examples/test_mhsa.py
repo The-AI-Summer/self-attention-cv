e68c8c9228dce65ba6ed7b1ca9eb49af04b2cef1
black0017 <black.adaloglou@gmail.com>
2021-01-31 23:32:28 +0100

    rename tests as examples. ignore idea files

--- examples/test_mhsa.py
+++ examples/test_mhsa.py
@@ -3,6 +3,8 @@ from self_attention import MultiHeadSelfAttentionAISummer
 
 model = MultiHeadSelfAttentionAISummer(64)
 x = torch.rand(16, 10, 64)  # [batch, tokens, dim]
-y = model(x)
+mask = torch.zeros(10, 64)  # tokens X tokens
+mask[5:8, 5:8] = 1
+y = model(x,mask)
 assert y.shape == x.shape
 print("MultiHeadSelfAttentionAISummer OK")
