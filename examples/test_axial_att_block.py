ec87663c4d0c56b117bb3ac01dd670e561709a63
black0017 <black.adaloglou@gmail.com>
2021-02-10 14:01:52 +0100

    Update test_axial_att_block.py

--- examples/test_axial_att_block.py
+++ examples/test_axial_att_block.py
@@ -2,7 +2,7 @@ import torch
 
 from self_attention_cv import AxialAttentionBlock
 
-model = AxialAttentionBlock(in_channels=256, dim=64)
+model = AxialAttentionBlock(in_channels=256, dim=64, heads=8)
 x = torch.rand(1, 256, 64, 64)  # [batch, tokens, dim, dim]
 
 y = model(x)
