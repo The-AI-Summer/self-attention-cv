983ae722b07f3cdb4685dad6c0d83f9144f89aae
black0017 <black.adaloglou@gmail.com>
2021-02-12 20:35:38 +0100

    fix: add CLS token for classification on ViT

--- self_attention_cv/vit/vit.py
+++ self_attention_cv/vit/vit.py
@@ -16,7 +16,7 @@ class ViT(nn.Module):
                  heads=4,
                  dim_linear_block=1024,
                  dim_head=64,
-                 dropout=0, transformer=None):
+                 dropout=0, transformer=None, classification=True):
         """
         Minimal reimplementation of ViT
         Args:
@@ -31,18 +31,27 @@ class ViT(nn.Module):
             dim_head: dim head in case you want to define it. defaults to dim/heads
             dropout: for pos emb and transformer
             transformer: in case you want to provide another transformer implementation
+            classification: creates an extra CLS token that we will index in the final classification layer
         """
         super().__init__()
         assert img_dim % patch_dim == 0, f'patch size {patch_dim} not divisible by img dim {img_dim}'
         self.p = patch_dim
+        self.classification = classification
         # tokens = number of patches
         tokens = (img_dim // patch_dim) ** 2
         token_dim = in_channels * patch_dim ** 2
 
         # Projection and pos embeddings
         self.project_patches = nn.Linear(token_dim, dim)
-        self.pos_emb1D = nn.Parameter(torch.randn(tokens, dim))
+
         self.emb_dropout = nn.Dropout(dropout)
+        if self.classification:
+            self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+            self.pos_emb1D = nn.Parameter(torch.randn(tokens + 1, dim))
+        else:
+            self.pos_emb1D = nn.Parameter(torch.randn(tokens, dim))
+
+        self.mlp_head = nn.Linear(dim, num_classes)
 
         if transformer is None:
             self.transformer = TransformerEncoder(dim, blocks=blocks, heads=heads,
@@ -52,21 +61,36 @@ class ViT(nn.Module):
         else:
             self.transformer = transformer
 
-        self.mlp_head = nn.Linear(dim, num_classes)
+    def expand_cls_to_batch(self, batch):
+        """
+        Args:
+            batch: batch size
+        Returns: cls token expanded to the batch size
+        """
+        return self.cls_token.expand([batch,-1,-1])
 
     def forward(self, img, mask=None):
+        batch_size = img.shape[0]
         # Create patches
         # from [batch, channels, h, w] to [batch, tokens , N], N=p*p*c , tokens = h/p *w/p
         img_patches = rearrange(img,
                                 'b c (patch_x x) (patch_y y) -> b (x y) (patch_x patch_y c)',
                                 patch_x=self.p, patch_y=self.p)
         # project patches with linear layer + add pos emb
-        patch_embeddings = self.emb_dropout(self.project_patches(img_patches) + self.pos_emb1D)
 
-        # feed patch_embeddings to transformer. shape: [batch, tokens, dim]
+        img_patches = self.project_patches(img_patches)
+
+        if self.classification:
+            img_patches = torch.cat( (self.expand_cls_to_batch(batch_size), img_patches), dim=1 )
+
+        patch_embeddings = self.emb_dropout( img_patches + self.pos_emb1D)
+
+        # feed patch_embeddings and output of transformer. shape: [batch, tokens, dim]
         y = self.transformer(patch_embeddings, mask)
 
-        # mean dim + final linear layer
-        return self.mlp_head(y.mean(dim=1))
+        # we index only the cls token for classification. nlp tricks :P
+        y = y[:, 0, :] if self.classification else y.mean(dim=1)
+
+        return self.mlp_head(y)
 
 
