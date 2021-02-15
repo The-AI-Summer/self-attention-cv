d3751ea37905c10e4bebd834192149f38340c3de
black0017 <black.adaloglou@gmail.com>
2021-02-15 01:39:32 +0100

    minor change to transunet

--- self_attention_cv/transunet/trans_unet.py
+++ self_attention_cv/transunet/trans_unet.py
@@ -8,33 +8,46 @@ from ..vit import ViT
 
 class TransUnet(nn.Module):
     def __init__(self, *, img_dim, in_channels, classes,
-                 vit_blocks=1,
+                 vit_blocks=12,
                  vit_heads=4,
-                 vit_dim_linear_mhsa_block=512,
+                 vit_dim_linear_mhsa_block=1024,
                  ):
+        """
+        My reimplementation of TransUnet based on the paper:
+        https://arxiv.org/abs/2102.04306
+        Badly written, many details missing and significantly differently
+        from the authors official implementation (super messy code also :P ).
+        My implementation doesnt match 100 the authors code.
+        Basically I wanted to see the logic with vit and resnet backbone for
+        shaping a unet model with long skip connections.
+
+        Args:
+            img_dim: the img dimension
+            in_channels: channels of the input
+            classes: desired segmentation classes
+            vit_blocks: MHSA blocks of ViT
+            vit_heads: number of MHSA heads
+            vit_dim_linear_mhsa_block: MHSA MLP dimension
+        """
         super().__init__()
         self.inplanes = 128
-        resnet_7x7_conv = True
+        vit_channels = self.inplanes * 8
 
-        if resnet_7x7_conv:
-            in_conv1 = nn.Conv2d(in_channels, self.inplanes, kernel_size=7, stride=2, padding=3,
-                                 bias=False)
-            bn1 = nn.BatchNorm2d(self.inplanes)
-            self.init_conv = nn.Sequential(in_conv1, bn1, nn.ReLU(inplace=True))
-            #self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
-        else:
-            self.init_conv = Bottleneck(in_channels, self.inplanes, stride=2)
+        # Not clear how they used resnet arch. since the first input after conv
+        # must be 128 channels and half spat dims.
+        in_conv1 = nn.Conv2d(in_channels, self.inplanes, kernel_size=7, stride=2, padding=3,
+                             bias=False)
+        bn1 = nn.BatchNorm2d(self.inplanes)
+        self.init_conv = nn.Sequential(in_conv1, bn1, nn.ReLU(inplace=True))
+        # self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
 
-
-        self.conv1 = Bottleneck(self.inplanes, self.inplanes*2,stride=2)
-        self.conv2 = Bottleneck(self.inplanes*2, self.inplanes*4, stride=2)
-        self.conv3 = Bottleneck(self.inplanes*4, self.inplanes*8,stride=2)
-
-        vit_channels = self.inplanes*8
+        self.conv1 = Bottleneck(self.inplanes, self.inplanes * 2, stride=2)
+        self.conv2 = Bottleneck(self.inplanes * 2, self.inplanes * 4, stride=2)
+        self.conv3 = Bottleneck(self.inplanes * 4, vit_channels, stride=2)
 
         self.img_dim_vit = img_dim // 16
         self.vit = ViT(img_dim=self.img_dim_vit,
-                       in_channels=vit_channels,  # based on resnet channels
+                       in_channels=vit_channels,  # encoder channels
                        patch_dim=1,
                        dim=vit_channels,  # vit out channels for decoding
                        blocks=vit_blocks,
@@ -48,18 +61,18 @@ class TransUnet(nn.Module):
         self.dec2 = Up(512, 128)
         self.dec3 = Up(256, 64)
         self.dec4 = Up(64, 16)
-        self.conv1x1 = nn.Conv2d(16,classes,kernel_size=1)
+        self.conv1x1 = nn.Conv2d(16, classes, kernel_size=1)
 
     def forward(self, x):
-        # ResNet 50 encoder
-        x2 = self.init_conv(x) # 128,64,64
+        # ResNet 50-like encoder
+        x2 = self.init_conv(x)  # 128,64,64
         x4 = self.conv1(x2)  # 256,32,32
-        x8 = self.conv2(x4) # 512,16,16
-        x16 = self.conv3(x8) # 1024,8,8
+        x8 = self.conv2(x4)  # 512,16,16
+        x16 = self.conv3(x8)  # 1024,8,8
         y = self.vit(x16)
         y = rearrange(y, 'b (x y) dim -> b dim x y ', x=self.img_dim_vit, y=self.img_dim_vit)
         y = self.vit_conv(y)
-        y = self.dec1(y, x8) # 256,16,16
+        y = self.dec1(y, x8)  # 256,16,16
         y = self.dec2(y, x4)
         y = self.dec3(y, x2)
         y = self.dec4(y)
